6f994d3cc91df4d49ced2cff91b7aca648b14dc7
henrique-pignatari <henrique.pignatarimoraes@gmail.com>
2024-01-30 17:40:22 -0300

    feat: create person instance from a string

--- modulo03/demo02-esmodules-internacionalization/src/index.js
+++ modulo03/demo02-esmodules-internacionalization/src/index.js
@@ -1,4 +1,5 @@
 import database from "./../database.json";
+import Person from "./person.js";
 import TerminalController from "./terminalController.js";
 
 const DEFAULT_LANG = "pt-BR";
@@ -9,13 +10,15 @@ terminalController.initializeTerminal(database, DEFAULT_LANG);
 
 async function mainLoop() {
   try {
-    const answer = await terminalController.question("What??");
+    const answer = await terminalController.question();
     if (answer === STOP_TERM) {
       terminalController.closeTerminal();
       console.log("process finished!");
       return;
     }
 
+    const person = Person.generateInstanceFromString(answer);
+    console.log(person.formatted(DEFAULT_LANG));
     return mainLoop();
   } catch (error) {
     console.log(error);
