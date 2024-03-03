d69f0e38586335596c1018c288fbd3c1360ef727
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-03-02 21:26:40 -0300

    feat: add observer pattern

---
+++ modulo07/behavioral/02-observer/jest.config.js
@@ -0,0 +1,28 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https:
+ */
+
+export default {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  maxWorkers: "50%",
+  testEnvironment: "node",
+
+  coverageReporters: [
+    "json",
+    "text",
+    "lcov",
+    "clover"
+  ],
+
+  coverageThreshold: {
+    global: {
+      branch: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+};
---
+++ modulo07/behavioral/02-observer/src/observers/marketing.js
@@ -0,0 +1,5 @@
+export default class Marketing {
+    update({ id, userName }) {
+        console.log(`[${id}]: [marketing] will send a wellcome email to [${userName}]`)
+    }
+}
